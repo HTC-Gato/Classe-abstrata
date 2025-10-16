7063f9345e10f4b9cf1e120759b38c93512220aa
HTC-Gato <heitort3ixeira@gmail.com>
2025-10-16 10:03:22 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,1 +1,18 @@
+package modelo;
+
+public abstract class Pessoa {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+    public abstract void imprimir();
+    
+}
 
