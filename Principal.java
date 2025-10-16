a98318c17273288ce7051b1f707400c189168b7f
HTC-Gato <heitort3ixeira@gmail.com>
2025-10-16 10:02:12 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,1 +1,32 @@
+package app;
+
+import modelo.Pessoa;
+import modelo.PessoaFisica;
+import modelo.PessoaJuridica;
+
+public class Principal {
+	
+	public static void main (String[] args) {
+		
+		PessoaFisica pf  = new PessoaFisica();
+		pf.setNome("Leandra");
+		pf.setCpf("1111111111");
+		
+		PessoaJuridica pj  = new PessoaJuridica();
+		pj.setNome("Leo");
+		pj.setCnpj("2222222222");
+		
+		Pessoa p = pf;
+		p.imprimir();
+		
+		System.out.println();
+		
+		p = pj;
+		p.imprimir();
+	}
+	public static void impressao(Pessoa pessoa) {
+		pessoa.imprimir();
+		
+	}
+}
 
