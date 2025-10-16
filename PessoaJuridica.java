74085477e9e1e5bfabc7933aeface930bfd18e15
HTC-Gato <heitort3ixeira@gmail.com>
2025-10-16 10:02:47 -0300

    Add PessoaJuridica class with CNPJ handling

--- PessoaJuridica.java
+++ PessoaJuridica.java
@@ -1,1 +1,25 @@
+package modelo;
+
+public class PessoaJuridica extends Pessoa {
+	
+	private String cnpj;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public void imprimir() {
+		System.out.println("Pesssoa Juridica");
+		System.out.println(getNome());
+		System.out.println(getCnpj());
+		
+	}
+	
+
+}
 
