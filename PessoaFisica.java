795059b200e54ae654b78d4fc538b198fcea8e4f
HTC-Gato <heitort3ixeira@gmail.com>
2025-10-16 10:02:58 -0300

    Add CPF attribute and methods to PessoaFisica class

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,1 +1,24 @@
+package modelo;
+
+public class PessoaFisica extends Pessoa {
+	
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public void imprimir() {
+		System.out.println("Pessoa Fisica");
+		System.out.println(getNome());
+		System.out.println(getCpf());
+	}
+	
+
+}
 
